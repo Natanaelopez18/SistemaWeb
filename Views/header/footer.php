8e8cfc148856e8668b40a76d25f9f53a74782c71
Natanaelopez18 <natanaelopezo@gmail.com>
2020-11-02 12:20:34 -0600

    Sistema de Alquiler de Peliculas

---
+++ Views/header/footer.php
@@ -0,0 +1,16 @@
+
+ <div class="container" style="background-color: #fff; border-radius: 3px; padding: 7px; margin-top:15px;">
+
+<footer>
+	<center>
+		<small><i>Copyright (c) <?php echo date('Y'); ?> <a href="" target="_blanck"> Natanael Lopez.</a></i></small>
+	</center>
+</footer>
+</div>
+<script src="assets/js/jquery.min.js"></script>
+<script src="assets/chosen/chosen.jquery.min.js"></script>  
+<script src="assets/js/chosen.js"></script>  
+<script src="assets/js/menu.js"></script>  
+<script src="assets/js/bootstrap.min.js"></script>	
+</body>
+</html>
